422bd05e381cb296fdb70bf3b0e3cd2c1a05ec7f
luisadsfatec@hotmail.com <luisadsfatec@hotmail.com>
2022-12-28 10:48:36 -0300

    configurações iniciais do projeto

---
+++ app.js
@@ -0,0 +1,25 @@
+//Carregando módulos
+
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+//const mongoose = require('mongoose')
+
+//Configurações
+    // Body Parser
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json())
+
+    // Handlebars
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('View engine', 'handlebars')
+
+    //Mongoose
+//Rotas
+
+//Outros
+const PORT = 8081
+app.listen(PORT, () =>{
+    console.log("Servidor rodando na porta "+ PORT)
+})
